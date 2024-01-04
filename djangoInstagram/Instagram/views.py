4857946433c7be2634754e83f8e12eebf13f3e72
jming623 <jming95623@gmail.com>
2024-01-04 23:04:02 +0900

    3일차 Bootstrap + Navbar

--- djangoInstagram/Instagram/views.py
+++ djangoInstagram/Instagram/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 
+class Iu(APIView):
+    def get(self, request):
+        print('GET으로 호출')
+        return render(request, "instagram/iu.html")
+
 class Sub(APIView):
     def get(self, request):
         print('GET으로 호출')
@@ -9,3 +14,4 @@ class Sub(APIView):
     def post(self, request):
         print('POST로 호출')
         return render(request, "instagram/main.html")
+
